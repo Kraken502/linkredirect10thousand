9b096946cec220f5b3099237e95ce0d9c8242ad6
Kraken502 <royality_brazmiality@ukr.net>
2025-06-20 20:31:48 +0300

    Delete redirect.js

--- redirect.js
+++
@@ -1,7 +0,0 @@
-// Получаем uid из URL
-const uid = window.location.pathname.split('/').pop().replace('.html', '');
-
-// ССЫЛКА ДЛЯ РЕДИРЕКТА — меняется только здесь!
-const baseUrl = "https://achieveyourvictories.com/kG6KC1w5?param=tqv15_mejit&aff_sub=10sale";
-
-window.location.href = baseUrl + encodeURIComponent(uid);
