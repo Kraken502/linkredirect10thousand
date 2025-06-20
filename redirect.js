6dbcedc3738998db9ee46871338ef91768ddea67
Kraken502 <royality_brazmiality@ukr.net>
2025-06-20 20:00:20 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,6 +2,6 @@
 const uid = window.location.pathname.split('/').pop().replace('.html', '');
 
 // ССЫЛКА ДЛЯ РЕДИРЕКТА — меняется только здесь!
-const baseUrl = "https://your-cpa-offer.com/?traffic_id=";
+const baseUrl = "https://achieveyourvictories.com/kG6KC1w5?param=tqv15_mejit&aff_sub=10sale";
 
 window.location.href = baseUrl + encodeURIComponent(uid);
